f356bbbdfc823fa1ea7857e7ee8525d324320675
unknown <luchomame@gmail.com>
2020-09-29 18:53:48 -0400

    removed keys

--- sentiment2.py
+++ sentiment2.py
@@ -9,10 +9,10 @@ import numpy as np
 '''
 DON'T TOUCH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 '''
-consumer_key = 'MNv539z4C74XY7Ic4nl8dEpfd'
-consumer_secret = 'ZJwNtgVEK3o2yy9tEIWmwbMhVkpmtmZtRK4uz3QtvyQNYIbxLF'
-access_token = '1223716040548417539-REnwuuX2f2rvbgaqizIiOdpCpqAM6A'
-access_token_secret = 'g2sKZzMYUiqAx5zHPWDX9jLO7S889NhtwaxCF51egpD3e'
+consumer_key = 'INSERT YOUR KEYS'
+consumer_secret = 'INSERT YOUR KEYS'
+access_token = 'INSERT YOUR KEYS'
+access_token_secret = 'INSERT YOUR KEYS'
 
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
